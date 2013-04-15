09104c07aa9d4865b40cdeed7d8b9de8557a6471
John Brewer <jbrewer@jera.com>
2013-04-15 14:40:32 -0700

    Stubs for AR2 demo.

---
+++ CVFAugmentedReality2.h
@@ -0,0 +1,13 @@
+//
+//  CVFAugmentedReality2.h
+//  CVFunhouse
+//
+//  Created by John Brewer on 4/11/13.
+//  Copyright (c) 2013 Jera Design LLC. All rights reserved.
+//
+
+#import "CVFImageProcessor.h"
+
+@interface CVFAugmentedReality2 : CVFImageProcessor
+
+@end
